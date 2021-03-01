58fbc69c0acc62add715915008504fba0ebd8aa2
RThymer <1012538928@qq.com>
2021-03-01 23:08:46 +0800

    侧边栏支持响应式收缩了，支持移动端展示

--- src/config/router.js
+++ src/config/router.js
@@ -1,12 +1,16 @@
+import { AccountBookOutlined, DatabaseOutlined } from "@ant-design/icons";
+
 export default [
   {
     path: "/about",
     name: "about",
     component: () => <div>about</div>,
+    icon: AccountBookOutlined,
   },
   {
     path: "/product",
     name: "product",
     component: () => <div>product</div>,
+    icon: DatabaseOutlined,
   },
 ];
--- src/layout/BasicLayout.js
+++ src/layout/BasicLayout.js
@@ -7,19 +7,37 @@ import router from "../config/router";
 import "./BasicLayout.css";
 
 class BasicLayout extends React.PureComponent {
+  state = {
+    collapsed: false,
+  };
+
   render() {
     const { children } = this.props;
+    const { collapsed } = this.state;
     const { Sider, Content } = Layout;
 
     return (
       <Layout>
-        <Sider theme="light">
+        <Sider
+          theme="light"
+          breakpoint="xs"
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(collapsed) => this.setState({ collapsed })}
+        >
           <div className="layout-header">
-            <span className="layout-header-title">RThymer's Here</span>
-            <Menu theme="light" mode="vertical">
+            <span className="layout-header-title">
+              {collapsed ? "Home" : "RThymer's Home"}
+            </span>
+            <Menu theme="light" mode="inline">
               {router.map((route) => (
                 <Menu.Item>
-                  <Link to={route.path}>{route.name}</Link>
+                  <Link to={route.path}>
+                    <div className="layout-nav-item">
+                      <route.icon />
+                      {!collapsed && <div>{route.name}</div>}
+                    </div>
+                  </Link>
                 </Menu.Item>
               ))}
             </Menu>
